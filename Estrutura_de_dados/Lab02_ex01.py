9c059fd3a49045ed83c425d381d52aaeb1ba4075
DDricko <rodrigowaltmann.n@gmail.com>
2024-06-19 17:23:34 -0300

    Exercícios do módulo de estrutura de dados

---
+++ Estrutura_de_dados/Lab02_ex01.py
@@ -0,0 +1,16 @@
+filmes = [
+    "The Shawshank Redemption",
+    "The Godfather",
+    "The Dark Knight",
+    "The Godfather Part ll",
+    "12 Angry Men",
+    "Schindler's List",
+    "The Lord of the Rings: The Return of the King",
+    "Pulp Fiction",
+    "The Lord of the Rings: The Fellowship of the Ring",
+    "The Good, the Bad and the Ugly"
+]
+
+filmes.pop(0)
+filmes.insert(1, "The Shawshank Redemption")
+print(filmes)
